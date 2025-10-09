bccb4d403cfc42a9bed60a193b901ca2bcb0ae00
fa-sharp <farshadkt@gmail.com>
2025-10-08 22:48:17 -0400

    add stripe example

---
+++ examples/stripe.rs
@@ -0,0 +1,42 @@
+//! Stripe webhook example. You can test this locally with the Stripe CLI:
+//!
+//! ```stripe listen -f http://localhost:8000/api/webhook/stripe```
+//!
+//! ```STRIPE_WEBHOOK_SECRET=xxxxx cargo run --features stripe --example stripe```
+
+use std::env;
+
+use rocket::{launch, post, routes, serde::json::Value};
+use rocket_webhook::{
+    RocketWebhook, RocketWebhookRegister, WebhookPayload, webhooks::built_in::StripeWebhook,
+};
+use serde::Deserialize;
+
+#[launch]
+fn rocket() -> _ {
+    let webhook_secret =
+        env::var("STRIPE_WEBHOOK_SECRET").expect("Env var STRIPE_WEBHOOK_SECRET is not set");
+    let webhook = RocketWebhook::builder()
+        .webhook(StripeWebhook::builder().secret_key(webhook_secret).build())
+        .max_body_size(10 * 1024)
+        .build();
+
+    let rocket = RocketWebhookRegister::new(rocket::build())
+        .add(webhook)
+        .register();
+
+    rocket.mount("/api", routes![stripe_endpoint])
+}
+
+#[derive(Debug, Deserialize)]
+pub struct StripeEvent {
+    #[allow(unused)]
+    data: Value,
+    #[serde(rename = "type")]
+    type_: String,
+}
+
+#[post("/webhook/stripe", data = "<payload>")]
+async fn stripe_endpoint(payload: WebhookPayload<'_, StripeEvent, StripeWebhook>) {
+    rocket::info!("Received event type: {:?}", payload.data.type_);
+}
